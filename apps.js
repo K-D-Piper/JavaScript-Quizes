98b1b44d600dbb146b12c79d19c212f2d1fe835e
K-D-Piper <kirsten.piper@mscoding.org>
2024-08-13 16:06:02 -0500

    Created Quiz Page. Linked. Added Day One Problems
    Created quizzes page and linked. Added  Day One and possibly day two's problems.

---
+++ apps.js
@@ -0,0 +1,109 @@
+//DAY ONE QUIZ 7/29/2024 : START
+//!TEMPATURE CONVERTER 
+
+//F = C * 9/5 +32
+// CONSOLE.LOG(ANSWER)
+
+let ctemp = 30
+let ftemp = ctemp * 9/5 + 32
+console.log(ftemp)
+
+//! Age calculator 
+//subtract the birth year from the current year to get the age. 
+// Console.log(answer)
+
+let currentyear = 2024
+let birthyear= 1998
+let age = currentyear - birthyear
+console.log(age)
+
+//TODO Personal Greeting
+//? Input the user's name. 
+// console out a personalized greeting message. 
+
+let greeting = "Heyyy. Want some cake?"
+console.log(greeting)
+
+//TODO Tip Calcualtor 
+//? Input the user to input the bill amout and the tip percetage.
+// Calcuate the tip amout and console the total bill.
+
+let billamount = 50
+let tipPercertage = .20
+let tip = billamount * tipPercertage
+console.log(tip)
+let finalbill = billamount + tip
+console.log(finalbill)
+
+//TODO Age Calculator in days
+//Ask the user to input their age in years
+//Multiply the age in years by 365
+//console the total days 
+
+let MyAge =  25
+let Days = 365
+let TotalDays = MyAge * Days
+console.log(TotalDays)
+
+//QUIZZES DAY ONE PART ONE : END 
+//QUIZZES DAY ??? : START
+
+
+//! Access Array Elements
+//TODO Define an array of items 
+let items = ["apple" , "banana" , "cherry" , "date", "elderberry"]
+
+//TODO Access specific elements
+let firstItem = items[0]
+let thirdItem = items[2]
+let lastItem = items[4]
+
+//Output the results
+console.log("Items: " + items)
+console.log("First Item: " + firstItem)
+console.log("Third Item: " + thirdItem)
+console.log("Last Item: " + lastItem)
+
+//! List of Favorite Movies
+//TODO Define an array of favorite movies
+//Output each movie individually
+
+fav_movies = ["Barbie Movie", "Coraline", "Princess & the Frog"]
+console.log(fav_movies[0])
+console.log(fav_movies[1])
+console.log(fav_movies[2])
+
+//Sum of Array Elements
+//TODO Define an array of numbers
+//TODO Calculate sum of the numbers 
+//Output the results
+
+let count_array = [3, 6, 9, 12, 15]
+let count_sum = 0
+
+for (let i = 0; i< count_array.length; i++) {
+    count_sum += count_array[i];
+}
+console.log(count_sum)
+
+//! Concatenate Strings in an Array 
+//TODO Define an array of strings
+//TODO Concateate the strings
+//Output the result 
+
+let cat_string = ["cats " , "are " , "cute ", "but ", "evil "]
+console.log(cat_string[0]+cat_string[1]+ cat_string[2] + cat_string[3] + cat_string[4])
+
+//! Update Array Elements
+//TODO Define an array of items
+let colors = ["red" , "blue" , "green" , "yellow", "purple"]
+//TODO Update
+//? green to cyan 
+//? purple to orange 
+//Output the result 
+
+colors[2] = "cyan"
+colors[4] = "orange"
+console.log(colors)
+
+// QUIZZES DAY ONE PART TWO: END 
