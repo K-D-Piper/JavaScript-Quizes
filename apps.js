bc824665970ab080bd7fb580e2a428a11d5aa31d
K-D-Piper <kirsten.piper@mscoding.org>
2024-08-15 12:41:50 -0500

    Updated Day 4 (8/2/2024) Quiz
    Added Problems and solutions for Day 4 quizzes

--- apps.js
+++ apps.js
@@ -196,44 +196,60 @@ if(sign_num ===50) {
 
 //DAY 3 (8/1/2024) QUIZ END
 //DAY 4 (8/2/2024) QUIZ START
+// JavaScript Quiz. 8/2/2024
+ 
+//Question 1:
+//Write a line of code that declares an array called fruits with the elements "apple", "banana", and "cherry". Then, log the second element of the array to the console.
+ //Q1
+ let quizFruits = ["apple" , "banana" , "cherry"]
+ console.log(quizFruits[1])
+ 
+ 
+ //Question 2:
+ //Write a line of code that declares an array called numbers with the elements 1, 2, 3, 4, and 5. Then, create a variable sum that is the sum of the first and third elements of the array, and log sum to the console.
+ //Q2
+ let numbers = [1 , 2 , 3 , 4, 5]
+ let varsum = numbers[0] + numbers[2]
+ console.log (varsum)
+  
+ //Question 3:
+ //Write a line of code that declares an array called languages with the elements "JavaScript", "Python", and "Java". Then, change the second element of the array to "Ruby" and log the entire array to the console.
+ //Q3 
+ 
+ let languages = ["JavaScript" , "Python" , "Java"]
+ languages[1] = "Ruby"
+ console.log(languages)
+ 
+  
+ //Question 4:
+ //Write a line of code that declares a variable age and assigns it the value 20. Then, use an if-else statement to check if age is greater than or equal to 18, and log "Eligible to vote" if true, and "Not eligible to vote" otherwise.
+ //Q4
+ 
+ let qAge = 20
+     
+ if (qAge >= 18) {
+     console.log("Eligible to Vote")
+ } else{
+     console.log("Sorry Kid. Not Eligable to Vote. ")
+ } 
+ 
+ //Question 5:
+ //Write a line of code that declares an array called cities with the elements "New York", "Los Angeles", and "Chicago". Then, create a variable lastCity that holds the last element of the array, and log lastCity to the console.
+  //Q5
+ 
+ let cities = ["New York" , "Los Angeles" , "Chicago"]
+ let lastcity = cities[2]
+ console.log(lastcity)
+ 
+ //Question 6:
+ //Write a line of code that declares a variable x and assigns it the value 7. Then, use an if-else statement to check if x is greater than 5 or less than 10, and log "In range" if true, and "Out of range" otherwise.
+ //Q6 
+ let x = 7
+ 
+ if (x > 5 || x < 10) {
+     console.log ("In range")
+ } else {
+     console.log ("Out of range")
+ }
 
-
-//Q1
-let fruits = ["apple" , "banana" , "cherry"]
-console.log(fruits[1])
-
-//Q2
-let numbers = [1 , 2 , 3 , 4, 5]
-console.log (numbers[0]+ numbers[2])
-
-//Q3  (come back)
-
-let languages = ["JavaScript" , "Python" , "Java"]
-let language.push = ["Ruby"]
-console.log(languages[1,4,3])
-
-
-//Q4
-
-let age = 20
-    
-if (age >= 18) {
-    console.log("Eligible to Vote")
-} else{
-    console.log("Sorry Kid. Not Eligable to Vote. ")
-}
-
-//Q5
-
-let cities = ["New York" , "Los Angeles" , "Chicago"]
-let lastcity = cities[2]
-console.log(lastcity)
-
-//Q6 
-let x = 7
-
-if (x > 5 || x < 10) {
-    console.log ("In range")
-} else {
-    console.log ("Out of range")
-}
+ // 8/2/2024 JavaScript Quiz END 
